e0b9ba999e023aacf9039f7937690cb4f5f316cc
Deepa-Universe <deepa.miracles@gmail.com>
2020-01-06 06:51:37 +0530

    First JS File

---
+++ main.js
@@ -0,0 +1,6 @@
+//This is my first Javascript file.
+//It is an external file.
+//Purpose of this file is to have a distributed load on the application. On HTML Page, there must be only HTML Code.
+//Important best practice is to place the address of this JS File after body tag in HTML Page.
+
+Console.log("Hello Javascript. From External File");
